b43275cb1becb88e4c85386036b8f68f6ea387ad
yuonny <dvkim@ucdavis.edu>
2025-02-22 16:00:16 -0800

    working urdf path

--- ros2/telekinesis/telekinesis/pyrun.py
+++ ros2/telekinesis/telekinesis/pyrun.py
@@ -1,14 +1,19 @@
 import pybullet as p
 import pybullet_data
+import os
 
 # Start PyBullet in GUI mode
 p.connect(p.GUI)
+
+path_src = os.path.abspath(__file__)
+path_src = os.path.dirname(path_src)
+path_src = os.path.join(path_src, "../robot_hand", "robot_pybullet.urdf")
 
 # (Optional) Set PyBullet’s search path for built-in URDFs
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 # Load your URDF file
-robot_id = p.loadURDF("ros2/telekinesis/robot_hand/robot.urdf", basePosition=[0, 0, 0], useFixedBase=True)
+robot_id = p.loadURDF(path_src, basePosition=[0, 0, 0], useFixedBase=True)
 
 # Run simulation
 while True:
